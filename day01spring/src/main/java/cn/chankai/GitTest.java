fc76f102ef9d2daf9bf11669b6162a8ebaad4913
chenkai <scchenkai@outlook.com>
2019-06-23 15:20:42 +0800

    git测试本地提交02

--- day01spring/src/main/java/cn/chankai/GitTest.java
+++ day01spring/src/main/java/cn/chankai/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
 
         System.out.println("git测试提交");
 
-        System.out.println("测试不commit能否提交远程仓库");
+        System.out.println("测试不commit能否提交远程仓库"); //不能,必须先提交到本地仓库,然后远程
+
+        System.out.println("测试branch");
     }
 }
