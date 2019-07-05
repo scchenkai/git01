5bd32033cee3dc7d50695529591422166adf347b
chenkai <scchenkai@outlook.com>
2019-07-05 22:42:25 +0800

    commmit by web02

---
+++ day01spring/src/main/java/cn/chankai/JuneGit.java
@@ -0,0 +1,16 @@
+package cn.chankai;
+
+import sun.applet.Main;
+
+/**
+ * 红色代表没有被追踪,绿色代表被追踪没有提交,处于暂存区,蓝色代表修改后没提交
+ */
+public class JuneGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("测试第一次提交 by idea");
+    }
+
+
+}
