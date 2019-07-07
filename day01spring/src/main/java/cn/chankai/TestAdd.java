6937aa2dbd48b919034e13413441d58287c837df
chenkai <scchenkai@outlook.com>
2019-07-07 15:03:47 +0800

    测试提交是否同时push了

---
+++ day01spring/src/main/java/cn/chankai/TestAdd.java
@@ -0,0 +1,15 @@
+package cn.chankai;
+
+public class TestAdd {
+
+    public static void main(String[] args) {
+        System.out.println("测试reset01");
+
+        System.out.println("测试reset02");
+
+        System.out.println("测试reset03");
+
+
+
+    }
+}
