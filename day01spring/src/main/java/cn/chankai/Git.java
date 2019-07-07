209c021f9f8af9e4c62ce32ab3c44f2064107f73
chenkai <scchenkai@outlook.com>
2019-07-07 17:51:03 +0800

    commit file and push

--- day01spring/src/main/java/cn/chankai/Git.java
+++ day01spring/src/main/java/cn/chankai/Git.java
@@ -5,6 +5,6 @@ public class Git {
     public static void main(String[] args) {
 
         System.out.println("没有加入管理是红色,加入是绿色,修改是蓝色");
-        System.out.println("");
+        System.out.println("commit and push");
     }
 }
