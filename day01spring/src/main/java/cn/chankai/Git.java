ab31e1e745efc2ab0739e853aad21ed3a79b7591
chenkai <scchenkai@outlook.com>
2019-07-07 17:48:53 +0800

    commit file

---
+++ day01spring/src/main/java/cn/chankai/Git.java
@@ -0,0 +1,10 @@
+package cn.chankai;
+
+public class Git {
+
+    public static void main(String[] args) {
+
+        System.out.println("没有加入管理是红色,加入是绿色,修改是蓝色");
+        System.out.println("");
+    }
+}
