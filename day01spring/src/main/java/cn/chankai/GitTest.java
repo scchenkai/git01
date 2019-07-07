7511de17d12061ece1e1f08180c872063a674ab6
chenkai <scchenkai@outlook.com>
2019-07-07 17:44:53 +0800

    Merge branch 'Mater02' into master04
    # Conflicts:
    #	day01spring/src/main/java/cn/chankai/GitTest.java

--- day01spring/src/main/java/cn/chankai/GitTest.java
+++ day01spring/src/main/java/cn/chankai/GitTest.java
@@ -17,11 +17,27 @@ public class GitTest {
 
         System.out.println("好烦");
 
+        System.out.println("测试当前的compare with current repository");
+
+        System.out.println("compare to test");
+        System.out.println("我是hello分支,即将合并到master");
+
+        System.out.println("路漫漫而修远兮");
+
+        System.out.println("提交回退测试");
+
+        System.out.println("提交web02");
+
+
+        System.out.println("好烦");
+
         System.out.println("测试回退02");
 
         System.out.println("测试提交01");
 
         System.out.println("测试提交02");
+
+        System.out.println("你好啊");
 
 
     }
