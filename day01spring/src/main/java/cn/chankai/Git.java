f6dc9b3b4e96afb29cf3948b40402c42ada5b8d3
chenkai <scchenkai@outlook.com>
2019-07-07 17:54:12 +0800

    annotate is msg about author and file change time.

--- day01spring/src/main/java/cn/chankai/Git.java
+++ day01spring/src/main/java/cn/chankai/Git.java
@@ -7,5 +7,7 @@ public class Git {
         System.out.println("没有加入管理是红色,加入是绿色,修改是蓝色");
         System.out.println("commit and push");
         System.out.println("git 可以使用patch补丁");
+
+        System.out.println("annotate 是当前作者注释和time");
     }
 }
